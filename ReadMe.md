fe2ff4271c2c99032e0033b5ed6ecba7e37a4b42
Detlef Steuer <detlef.steuer@gmx.de>
2020-02-05 23:47:29 +0100

    Restart of CRAN2OBS, moved R-package to subdir, added script for automating creation of new packages

---
+++ ReadMe.md
@@ -0,0 +1,24 @@
+# CRAN2OBS
+
+This project tries to automate, as far as possible, the complete
+mirroring of CRAN into OBS.
+
+This is not even work-in-progress, it is just the beginning of some work.
+
+## External Resources
+
+- osc , interface to OBS
+- R2spec , generates spec files from R packages description
+- rpmbuild , low level rpm build tool, used by R2spec
+
+## Scripts
+
+In scripts/ are shell scripts to help with automation
+
+## R-Package
+
+In a supplementary package is explore if a replacement of external
+scripts with an R package is possible.
+
+Furthermore such a package should help to install R-*.rpm packages
+automatically as an enhancemnt to {install|update|etc.}.packages().
---
+++ Scripts/rp2sp.sh
@@ -0,0 +1,59 @@
+#!/bin/bash 
+# first argument is the CRAN-name of a new package
+# Must:  ~/.oscrc contains valid username for OBS
+# R2spec must be installed
+# osc must be installed
+
+PACK=$1
+# PACK may not already exist in OBS
+
+
+function setup {
+    cd ~/OBS/devel\:languages\:R\:released
+    # change into OBS directory
+
+    osc mkpac R-$PACK
+    cd R-$PACK
+    R2spec --no-check --no-suggest -p $PACK
+    cp ~/rpmbuild/SPECS/R-$PACK.spec .
+    cp ~/rpmbuild/SOURCES/${PACK}*.tar.gz .
+    # Testbuild
+    osc build R-$PACK.spec
+    cd -
+}
+
+function build {
+    cd ~/OBS/devel\:languages\:R\:released/R-$PACK
+    osc build --no-init R-$PACK.spec
+    cd -
+}
+
+function push {
+    # Manual correction of filelist, may be multiple cycles
+    # maybe osc --noinit in subsequent runs
+    # check R2rpm
+    # if finally created
+    cd ~/OBS/devel\:languages\:R\:released/R-$PACK
+    
+    osc add R-$PACK.spec ${PACK}*.tar.gz
+    MSG="initial build version ` grep Version: *spec | gawk -- '{print $2}'`"
+    osc vc -m "$MSG"
+    osc ci -m "$MSG"
+
+    cd -
+}
+
+if [ $2 = "setup" ]
+then
+    setup
+fi
+
+if [ $2 = "build" ]
+then
+    build
+fi
+
+if [ $2 = "push" ]
+then
+    push
+fi
