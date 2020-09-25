aeece5b98860ccfb894f2683f28a9c7fd5cf103e
Detlef Steuer <detlef.steuer@hsu-hh.de>
2020-09-25 15:09:24 +0200

    reflect parameter name change in cran2repo (ap -> status)

--- R-Package/R/repo.R
+++ R-Package/R/repo.R
@@ -28,7 +28,7 @@ cran2repo <- function(cran=getOption("c2o.cran"),
         pkgs <- which(status$depLen == level)
         for (pkg in status$Package[pkgs]) {
             logger(paste0("* Working on ", pkg ))
-            result <- pkg2pac(pkg, localOBS=localOBS, remoteprj=remoteprj, ap=status,
+            result <- pkg2pac(pkg, localOBS=localOBS, remoteprj=remoteprj, status=status,
                               download.cache=download.cache, binary.cache=binary.cache, log=log)
             if (result$status == "done") {
                 logger(paste0("** Sync finished for ", pkg))
