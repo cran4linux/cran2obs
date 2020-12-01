0e75d000113005b2751beb8bfa0c2c0764230d4a
Detlef Steuer <detlef.steuer@hsu-hh.de>
2020-12-01 12:03:53 +0100

    Added counter and ETA estimator

--- R-Package/R/repo.R
+++ R-Package/R/repo.R
@@ -21,13 +21,18 @@ cran2repo <- function(cran=getOption("c2o.cran"),
                       download.cache = getOption("c2o.download.cache"),
                       binary.cache = getOption("c2o.binary.cache"),
                       log = getOption("c2o.logfile")){
-    
+
+    sync.start <- Sys.time()
     logger( paste0("Sync started at ", Sys.time() ))
     status <- read.table(statusfile, header=TRUE, sep=";", colClasses="character")
 
     actions <- defineActions(status)
     
     all.deplength <- sort(unique(as.numeric(status$depLen)))
+    total.to.build <- length(actions$update) + length(actions$totry)
+    start.time <- Sys.time()
+    build.counter <- 0
+    
     for (level in all.deplength){
         pkgs <- which(status$depLen == level)
         for (pkg in status$Package[pkgs]) {
@@ -40,12 +45,14 @@ cran2repo <- function(cran=getOption("c2o.cran"),
                 }
             }
             if (! is.na(buildtype))
-                {
-                    logger(paste0("* Working on ", pkg ))
-                    
-                } else {
-                    next
-                }
+            {
+                build.counter <- build.counter + 1
+                pkg.build.start <- Sys.time()
+                logger(paste0("* Working on ", pkg, "( ," build.counter, "/", total.to.build,")" ))
+                logger(paste0("  Begin:     ", pkg.build.start))
+            } else {
+                next
+            }
             num <- which(status$Package == pkg)
             result <- pkg2pac(pkg, localOBS=localOBS, remoteprj=remoteprj, statusfile=statusfile,
                               download.cache=download.cache, binary.cache=binary.cache, log=log)
@@ -64,10 +71,18 @@ cran2repo <- function(cran=getOption("c2o.cran"),
                 logger(paste0("** Sync failed for ", pkg))
             }
             status <- updateStatusOfpkg ( status, pkg, result, statusfile=statusfile, log=log) 
-            
+            pkg.build.end <- Sys.time()
+            logger( paste0("  Work on ",pkg, "ended at", pkg.build.end, "after ",
+                           floor(as.numeric(difftime(pkg.build.end, pkg.build.start, units="sec"))*100)/100, "s" ))
+            avg.time <- as.numeric(difftime(pk.build.end, sync.start,units="secs"))/build.counter
+            logger( paste0("  Average time per package:", floor(avg.time*100)/100, "s" ))
+            logger( paste0("  Expected remaining time:", floor((total.to.build-build.counter)*avg.time*100)/100, "s" ))
         }
     }
-    logger( paste0("Sync finished at ", Sys.time() ))
+    sync.end <- Sys.time()
+    logger( paste0("  Sync finished at ", sync.end, "after ",
+                   floor(as.numeric(difftime(sync.end, sync.start, units="auto"))*100)/100, "s" ))
+    
     return(status)
 }
                                         
